89f3d7813872e227dd753ac2e3f35590049bc85c
nbhusare <neerajbhusare@gmail.com>
2018-04-24 23:10:58 -0500

    Removed extra projects

--- org.neclipse.xtext.functiondsl/src/org/neclipse/xtext/functiondsl/scoping/FunctionImportedNamespaceAwareLocalScopeProvider.java
+++
@@ -1,8 +0,0 @@
-package org.neclipse.xtext.functiondsl.scoping;
-
-import org.neclipse.xtext.parentdsl.scoping.ParentImportedNamespaceAwareLocalScopeProvider;
-
-public class FunctionImportedNamespaceAwareLocalScopeProvider extends
-		ParentImportedNamespaceAwareLocalScopeProvider {
-
-}
--- org.neclipse.xtext.training.smallJava/src/org/neclipse/xtext/training/smallJava/validation/ValidationConstants.java
+++
@@ -1,9 +0,0 @@
-package org.neclipse.xtext.training.smallJava.validation;
-
-public interface ValidationConstants {
-
-	String INVALID_CLASS_NAME = "org.neclipse.xtext.training.smallJava.validation.InvalidEntityName";
-	String INVALID_ATTRIBUTE_NAME = "org.neclipse.xtext.training.smallJava.validation.InvalidAttributeName";
-	String INVALID_SUPER_CLASS = "org.neclipse.xtext.training.smallJava.validation.InvalidSuperClass";
-	String HIERARCHY_CYCLE = "org.neclipse.xtext.training.smallJava.validation.HierarchyCycle";
-}
